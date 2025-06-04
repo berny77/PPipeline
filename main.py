5a5860aec04c063e078d58365698b0d2570394d5
berny77 <berny.navarrolopez@ucr.ac.cr>
2025-06-03 18:42:00 -0600

    Primeros cambios

---
+++ main.py
@@ -0,0 +1,10 @@
+def es_palindromo(texto: str) -> bool:
+    texto = ''.join(c.lower() for c in texto if c.isalnum())
+    return texto == texto[::-1]
+
+if __name__ == "__main__":
+    entrada = input("Ingresa una palabra o frase: ")
+    if es_palindromo(entrada):
+        print("Es un palíndromo mi compa!")
+    else:
+        print("No es un palíndromo mi compa!")
---
+++ test_main.py
@@ -0,0 +1,8 @@
+from main import es_palindromo
+
+def test_palindromos_simples():
+    assert es_palindromo("oso")
+    assert es_palindromo("Ana")
+    assert es_palindromo("12321")
+
+
